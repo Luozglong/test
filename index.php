195f9ae1cde8d5e9781e99747399c61f85b86b91
Luozglong <117190084+Luozglong@users.noreply.github.com>
2024-12-22 16:14:53 +0700

    n

--- index.php
+++ index.php
@@ -3,4 +3,25 @@
 echo "Hello World! azure 2";
 include "test1.html";
 
+$connectionInfo = array("UID" => "adminroot", "pwd" => "Dttmcnd3@", "Database" => "data2", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:banhang3.database.windows.net,1433";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
+$query = "SELECT*from vandon";
+
+$queryresive = mysqli_query($conn,$query);
+
+while($querexcutr = mysqli_fetch_array($queryresive))
+{
+   
+  echo "<div class=\"conten2\">
+  <img class=\"one\" style=\"float: left;\" src=\" ".$querexcutr['imagelink']." \" width=\"50%\" height=\"auto\">
+  <h1>".$querexcutr['nguoinhan']."</h1>
+  <h2>".$querexcutr['dienthoai']."<h1>
+  <h3>".$querexcutr['diachi']."</h1>   
+  <h3>".$querexcutr['ngaygiaohang']."</h2> 
+  <br> *".$querexcutr['ghichu']."<br/>
+</div> ";
+
+}
+echo"</body></html>";
 ?>
