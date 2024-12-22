dce064ab5b4f3cd4c2a8db48efdba7d032720a72
Luozglong <117190084+Luozglong@users.noreply.github.com>
2024-12-22 16:22:18 +0700

    n

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <?php
 
-echo "Hello World! azure 2";
+echo "Hello World! azure 3";
 include "test1.html";
-
+try{
 $connectionInfo = array("UID" => "adminroot", "pwd" => "Dttmcnd3@", "Database" => "data2", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
 $serverName = "tcp:banhang3.database.windows.net,1433";
 $conn = sqlsrv_connect($serverName, $connectionInfo);
-$query = "SELECT*from vandon";
+$query = "SELECT*from dbo.vandon";
 
 $queryresive = mysqli_query($conn,$query);
 
@@ -19,9 +19,12 @@ while($querexcutr = mysqli_fetch_array($queryresive))
   <h2>".$querexcutr['dienthoai']."<h1>
   <h3>".$querexcutr['diachi']."</h1>   
   <h3>".$querexcutr['ngaygiaohang']."</h2> 
-  <br> *".$querexcutr['ghichu']."<br/>
+  <br>".$querexcutr['ghichu']."<br/>
 </div> ";
 
+}
+}catch{
+  echo"<div class=\"box\">heeloo 2</div>";
 }
 echo"</body></html>";
 ?>
